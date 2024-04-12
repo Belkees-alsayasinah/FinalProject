3a4f5959fa2aa1235b31b7896095d26ee648ad2c
belkees alsayasinah <sayasinah2017@gmail.com>
2024-04-12 21:19:23 +0300

    "Constant Edit"

--- lib/Style/constant.dart
+++ lib/Style/constant.dart
@@ -3,40 +3,18 @@ import 'package:flutter/material.dart';
 const Color white = Colors.white;
 const Color black = Colors.black;
 const Color red = Colors.red;
-const Color darkBlue = Color.fromRGBO(19, 26, 44, 1.0);
-const Color themeApp = Color.fromARGB(255, 222, 95, 236);
-const Color buttonTheme = Color.fromARGB(255, 170, 186, 162);
 const double appPadding = 30.0;
 Color buttonColor = Color(0xFF659B5E);
 Color buttonColorOpa = Color(0xFF96B990);
 Color textColor = Color(0xFF556F44);
 Color fieldColor = Color(0xFFE8E8E8);
 
-const Color firstBackColor = Color.fromARGB(255, 78, 21, 111);
-const Color secondBackColor = Color.fromARGB(255, 222, 95, 236);
-const Color thirdBackColor = Color.fromARGB(255, 186, 162, 168);
-const BoxDecoration gradienBackground = BoxDecoration(
-  gradient: LinearGradient(
-      begin: Alignment.topLeft,
-      end: Alignment.bottomRight,
-      colors: [
-        firstBackColor,
-        secondBackColor,
-        thirdBackColor,
-      ]
-  ),
-);
-
-
-const Color c = Color.fromARGB(255, 147, 122, 128);
-
 void printFullText(String text) {
   final Pattern = RegExp('.{1,800}');
   Pattern.allMatches(text).forEach((match) => print(match.group(0)));
 }
-
 late String token = '';
 
-//     Size size = MediaQuery.of(context).size;
+// Size size = MediaQuery.of(context).size;
 
 // {status: true, message: Registration done successfully, data: {email: barkes@gmail.com, name: barkes, phone: 0998055117, id: 16360, image: https://student.valuxapps.com/storage/assets/defaults/user.jpg, token: GadMj4gpqJGg2pgPvoD9QSmFPnP0Um92vR1b9wkjrYZCGvQtLdK7M9CIzlHhiz2303uYgs}}
---
+++ lib/service/info.dart
@@ -0,0 +1,8 @@
+
+class UserInformation {
+  static String user_token = '';
+  static String app_theme = "light";
+  static String app_lang = "en";
+  static String usertype = "user";
+  static int usre_id = 5;
+}
